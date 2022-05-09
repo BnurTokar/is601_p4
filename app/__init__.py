5f655a5114c7ead6e27b641efc2e4f96abfd73aa
beyzanurtokar <bt268@njit.edu>
2022-05-09 15:22:52 -0400

    Added logging_config blueprint into create_app

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ from app.transactions import transactions
 from app.cli import create_database
 from app.context_processors import utility_text_processors
 from app.db import db,database
+from app.config_logging import logging_config
 
 from app.db.models import User
 from app.exceptions import http_exceptions
@@ -56,6 +57,7 @@ def create_app():
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
     app.register_blueprint(database)
+    app.register_blueprint(logging_config)
     app.context_processor(utility_text_processors)
     app.register_blueprint(transactions)
     app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'Simplex'
@@ -69,9 +71,6 @@ def create_app():
     # add command function to cli commands
     app.cli.add_command(create_database)
 
-    # Deactivate the default flask logger so that log messages don't get duplicated
-    app.logger.removeHandler(default_handler)
-
     # get root directory of project
     root = os.path.dirname(os.path.abspath(__file__))
 
@@ -80,74 +79,6 @@ def create_app():
     # make a directory if it doesn't exist
     if not os.path.exists(upload_dir):
         os.mkdir(upload_dir)
-
-    # set the name of the apps log folder to logs
-    logdir = os.path.join(root, 'logs')
-    # make a directory if it doesn't exist
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-    # set name of the log file
-    log_file = os.path.join(logdir, 'info.log')
-
-    handler = logging.FileHandler(log_file)
-    # Create a log file formatter object to create the entry in the log
-    formatter = RequestFormatter(
-        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-        '%(levelname)s in %(module)s: %(message)s'
-    )
-    # set the formatter for the log entry
-    handler.setFormatter(formatter)
-    # Set the logging level of the file handler object so that it logs INFO and up
-    handler.setLevel(logging.INFO)
-    # Add the handler for the log entry
-    app.logger.addHandler(handler)
-
-    @app.before_request
-    def start_timer():
-        g.start = time.time()
-
-    @app.after_request
-    def log_request(response):
-        if request.path == '/favicon.ico':
-            return response
-        elif request.path.startswith('/static'):
-            return response
-        elif request.path.startswith('/bootstrap'):
-            return response
-
-        now = time.time()
-        duration = round(now - g.start, 2)
-        dt = datetime.datetime.fromtimestamp(now)
-        timestamp = rfc3339(dt, utc=True)
-
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        host = request.host.split(':', 1)[0]
-        args = dict(request.args)
-
-        log_params = [
-            ('method', request.method),
-            ('path', request.path),
-            ('status', response.status_code),
-            ('duration', duration),
-            ('time', timestamp),
-            ('ip', ip),
-            ('host', host),
-            ('params', args)
-        ]
-
-        request_id = request.headers.get('X-Request-ID')
-        if request_id:
-            log_params.append(('request_id', request_id))
-
-        parts = []
-        for name, value in log_params:
-            part = name + ': ' + str(value) + ', '
-            parts.append(part)
-        line = " ".join(parts)
-        #this triggers a log entry to be created with whatever is in the line variable
-        app.logger.info('this is the plain message')
-
-        return response
 
     return app
 
