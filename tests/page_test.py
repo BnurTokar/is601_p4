370ba868750374eb42e28ca547e85e1ffbf1e2f7
beyzanurtokar <bt268@njit.edu>
2022-05-09 20:34:37 -0400

    3) test login page link

--- tests/page_test.py
+++ tests/page_test.py
@@ -2,3 +2,5 @@ def test_main_menu_links(client):
     response =client.get('/')
     assert response.status_code == 200
     assert b'href="/about"' in response.data
+    assert b'href="/login"' in response.data
+
