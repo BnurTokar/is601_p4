8d205668ce341943a040faee2d95f691f0bdad4c
beyzanurtokar <bt268@njit.edu>
2022-05-09 20:52:24 -0400

    7) test home page

--- tests/page_test.py
+++ tests/page_test.py
@@ -13,3 +13,9 @@ def test_auth_pages(client):
     assert response.status_code == 200
     response = client.get("/login")
     assert response.status_code == 200
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Home" in response.data
