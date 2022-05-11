8f58aefc3ae6adf8a8fa34834c788290ad8c1b72
beyzanurtokar <bt268@njit.edu>
2022-05-11 00:37:17 -0400

    31) test production mode

--- tests/config_test.py
+++ tests/config_test.py
@@ -6,3 +6,7 @@ def test_development_config(application):
     application.config.from_object('app.config.DevelopmentConfig')
     assert not application.config['TESTING']
 
+def test_production_config(application):
+    application.config.from_object('app.config.ProductionConfig')
+    assert not application.config['TESTING']
+
