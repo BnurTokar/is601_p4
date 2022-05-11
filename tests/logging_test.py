f3d0477f9ea6948cc87895daa6951a302f5ba832
beyzanurtokar <bt268@njit.edu>
2022-05-10 23:53:14 -0400

    19) test for upload transaction log file

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -24,3 +24,11 @@ def test_each_request_response_log_file():
     assert absolute_file == "/home/myuser/app/logs/each_request_response.log"
     assert os.path.isfile(absolute_file)
 
+
+def test_upload_transaction_log_file ():
+
+    absolute_folder = config.Config.LOG_DIR_TEST
+    absolute_file = os.path.join(absolute_folder, filename_upload)
+
+    assert absolute_file == "/home/myuser/app/logs/upload_transaction.log"
+    assert os.path.isfile(absolute_file)
