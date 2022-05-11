a24de4974996e037c298faf93eb74ba8c53a5544
beyzanurtokar <bt268@njit.edu>
2022-05-10 23:50:32 -0400

    16) test for register functionality

--- tests/page_three_test.py
+++ tests/page_three_test.py
@@ -25,3 +25,22 @@ def test_for_login(application,client):
         db.session.delete(user)
         assert db.session.query(User).count() == 0
 
+
+def test_for_register(application,client):
+    with application.app_context():
+        assert db.session.query(User).count() == 0
+
+        user = User('beyzatest@test.com', 'testtest')
+        db.session.add(user)
+        assert user.email == 'beyzatest@test.com'
+        db.session.commit()
+        assert db.session.query(User).count() == 1
+
+        with application.test_client() as client:
+            response_register= client.post('/register', data=dict(email='beyzatest@test.com',password='testtest'), follow_redirects=True)
+
+        response = client.get("/register")
+        assert response.status_code == 200
+
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
