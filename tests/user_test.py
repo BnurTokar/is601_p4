37fbaac7130f1c65f6a8186f4706b31128129108
beyzanurtokar <bt268@njit.edu>
2022-05-10 23:56:58 -0400

    21) test for user related transaction, db added Credit, Debit deleted user query

--- tests/user_test.py
+++ tests/user_test.py
@@ -22,3 +22,25 @@ def test_user_query(application):
 
         db.session.delete(user)
         assert db.session.query(User).count() == 0
+
+
+def test_user_transaction_query(application):
+    with application.app_context():
+
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Transaction).count() == 0
+
+        email = 'beyzatest@test.com'
+        password = 'testtest'
+        user = User(email, password)
+
+        db.session.add(user)
+        assert db.session.query(User).count() == 1
+
+        user.transactions = [Transaction("100", "CREDIT", "100"), Transaction("200", "DEBIT", "100")]
+        assert db.session.query(Transaction).count() == 2
+
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Transaction).count() == 0
+
