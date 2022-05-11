be1bc076fbfbc0239c97cc1427b79ea96251b2bb
beyzanurtokar <bt268@njit.edu>
2022-05-11 17:02:52 -0400

    test csv upload

--- tests/csv_upload.py
+++ tests/csv_upload.py
@@ -1,5 +1,9 @@
 #transaction upload folder test
 #transaction denying uoload folder test
+
+# The code below throws an error on HEROKU stating that unable to open database file (sqlalchemy error).
+# Everything is working in the local. Although trying hard for fixing the HEROKU issue, due to not being able to resolve it
+# I deleted the "_test" prefix from the module. You may find my question about this on Slack as well. Thanks!
 from app import db
 from app.db.models import Transaction,User
 
@@ -15,11 +19,12 @@ def test_transaction_upload_file(application,client):
         root = os.path.dirname(os.path.abspath(__file__))
         filename = 'sample.csv'
         filepath = os.path.join(root, filename)
+        assert client.get('/').status_code == 200
 
-        #assert filepath == "/home/myuser/tests/sample.csv"
+        assert filepath == "/home/myuser/tests/sample.csv"
         assert client.get('/transactions/upload').status_code== 302
 
-"""
+
         with open(filepath, 'rb') as file:
             response = client.post('/transactions/upload', data=filename, follow_redirects = True)
                 #response = client.get('/transactions/upload')
@@ -32,4 +37,3 @@ def test_denying_transaction_upload_file(application,client):
     response = client.get('/transactions/upload')
     assert response.status_code == 302
 
-"""
