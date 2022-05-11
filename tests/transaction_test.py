aa7060abd94a159575a98f0309d5b7a97a838ff1
beyzanurtokar <bt268@njit.edu>
2022-05-11 00:01:22 -0400

    23) test for transaction db balance

---
+++ tests/transaction_test.py
@@ -0,0 +1,32 @@
+#test for transaction db balance
+#test for transaction db query
+
+from app import db
+from app.db.models import Transaction
+from calculator import Calculator
+
+def test_transaction_balance(application):
+    calculator_obj = Calculator()
+    transaction1 = Transaction("100", "CREDIT", "100")
+    db.session.add(transaction1)
+    db.session.commit()
+    balance = calculator_obj.add(transaction1.amount)
+    assert balance == 100
+
+    transaction2 = Transaction("80", "CREDIT", balance)
+    db.session.add(transaction2)
+    db.session.commit()
+    balance = calculator_obj.add(transaction2.amount)
+    assert balance == 180
+
+    transaction3 = Transaction("-20", "DEBIT", balance)
+    db.session.add(transaction3)
+    db.session.commit()
+    balance = calculator_obj.add(transaction3.amount)
+    assert balance == 160
+
+    db.session.delete(transaction1)
+    db.session.delete(transaction2)
+    db.session.delete(transaction3)
+
+
